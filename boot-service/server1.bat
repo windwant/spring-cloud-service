9e82073dab7a91b1077ab59a0af44af87266a7fb
windwant <nie.junlei@hotmail.com>
2018-08-15 17:55:15 +0800

    lockback

--- boot-service/server1.bat
+++ boot-service/server1.bat
@@ -1,1 +1,1 @@
-java -cp .;lib/*;spring-boot-service-0.0.1-SNAPSHOT.jar org.windwant.app.BootApplication --server.port=8222 --register.port=8111 --spring.application.name=provider
+java -cp .;lib/*;boot-service-0.0.1-SNAPSHOT.jar org.windwant.app.BootApplication --server.port=8222 --register.port=8111 --spring.application.name=provider
