b9a4da66adfc5d67674170a13af72c21aac585a2
windwant <nie.junlei@hotmail.com>
2018-08-16 17:22:22 +0800

    zipkin

---
+++ zipkin-server/src/main/java/org/windwant/zipkin/ZipkinApplication.java
@@ -0,0 +1,14 @@
+package org.windwant.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
